9c1b6bad09322bcf3f043b206ab5e9a272d2a06c
Himanshu.Pandey <hpandeycsebt@gmail.com>
2022-02-13 14:45:59 +0530

    added styling to orders component

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -33,9 +33,70 @@ export class OrdersComponent implements OnInit {
       },
       tableOrder: {
         id: 1,
-        persons: 4,
+        persons: 2,
+        arrival: '10:00 AM',
+        date: '14 Feb 2022',
+      },
+    },
+    {
+      id: 1,
+      restaurant: {
+        id: 1,
+        name: 'PizzaHut',
+        address: 'WTP, Jaipur',
+        image: 'https://thumbs.dreamstime.com/z/pizza-hut-logo-22575794.jpg',
+      },
+      foodOrder: {
+        id: 1,
+        items: [
+          {
+            title: 'Pizza - Margherita, Large',
+            price: 589,
+            quantity: 1,
+          },
+          {
+            title: 'Coca Cola',
+            price: 40,
+            quantity: 1,
+          },
+        ],
+      },
+      tableOrder: {
+        id: 1,
+        persons: 2,
         arrival: '5:00 PM',
         date: '10 Feb 2022',
+      },
+    },
+    {
+      id: 1,
+      restaurant: {
+        id: 1,
+        name: 'McDonalds',
+        address: 'Silvercity, Dehradun',
+        image:
+          'https://cdn.pixabay.com/photo/2016/04/20/00/41/mcdonalds-1340199_1280.jpg',
+      },
+      foodOrder: {
+        id: 1,
+        items: [
+          {
+            title: 'Big Mac',
+            price: 199,
+            quantity: 4,
+          },
+          {
+            title: 'Fries',
+            price: 89,
+            quantity: 4,
+          },
+        ],
+      },
+      tableOrder: {
+        id: 1,
+        persons: 4,
+        arrival: '6:00 PM',
+        date: '12 Feb 2022',
       },
     },
   ];
