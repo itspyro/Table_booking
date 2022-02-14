2a1454e32d72a20120ed8fdd620669c68deb534c
Mradul.Pandey <pp898235@gmail.com>
2022-02-14 14:10:10 +0530

    login/signUp

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DailogComponent } from './dailog/dailog.component';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements DailogComponent {
+
+  constructor(private readonly dialog: MatDialog) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  openDialog() {
+    console.error("Openning!");
+    this.dialog.open(DailogComponent);
+    console.error("Openning!");
+  }
+
+}
