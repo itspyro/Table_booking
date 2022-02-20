ee0e186b2f885c63d3f1c7e285331f6a2540e13d
Aakriti Sahrawat <sahrawataakriti2@gmail.com>
2022-02-20 19:54:37 +0530

    added location in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router, NavigationEnd  } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  curr_url:string="";
+
+  constructor( private router: Router) {} 
+  ngOnInit() {
+      this.router.events.subscribe(e => {
+        if (e instanceof NavigationEnd) {
+          this.curr_url=e.url;
+        }
+      });
+  } 
 }
