bed04f20ef202a7107e43fab21d3968b125876a4
Himanshu.Pandey <hpandeycsebt@gmail.com>
2022-02-22 10:53:27 +0530

    Updated auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Cuisine } from 'app/services/cuisine.model';
 import { RestaurantService } from 'app/services/restaurants.service';
+import { Subscription } from 'rxjs';
 import { DailogComponent } from './dailog/dailog.component';
 
 @Component({
@@ -9,22 +10,28 @@ import { DailogComponent } from './dailog/dailog.component';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent implements DailogComponent {
+export class AuthComponent implements OnInit, OnDestroy {
   cuisines: string[] = [];
+  subscription?: Subscription;
 
   constructor(private restaurantService: RestaurantService) {}
   ngOnInit(): void {
     this.restaurantService.getCuisines();
-    this.restaurantService.cuisineList.subscribe((cuisines: Cuisine[]) => {
-      cuisines.map((cuisine) => {
-        if (this.cuisines.includes(cuisine.cuisineName)) {
-          return;
-        }
-        this.cuisines.push(cuisine.cuisineName);
-      });
-    });
+    this.subscription = this.restaurantService.cuisineList.subscribe(
+      (cuisines: Cuisine[]) => {
+        cuisines.map((cuisine) => {
+          if (this.cuisines.includes(cuisine.cuisineName)) {
+            return;
+          }
+          this.cuisines.push(cuisine.cuisineName);
+        });
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
   onLoginButton(loginInfo) {
     console.log(loginInfo);
   }
