6387d73682d1f258c98c1c40cad30afadd8fb291
VismayTandel <45883257+VismayTandel@users.noreply.github.com>
2022-02-24 14:55:49 +0530

    api url added in environment

--- src/app/services/userprofile.service.ts
+++ src/app/services/userprofile.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 import { Subject } from 'rxjs';
 import { Restaurant } from './restaurant.model';
 import { Table } from './table.model';
@@ -17,8 +18,12 @@ export class UserprofileService {
   constructor(private http: HttpClient) { }
 
   getUser() {
-    const url = 'http://localhost:8080/api/user/find/1';
-    this.http.get<{ httpStatusCode: number, responseMessage: string, users: User }>(url)
+    const USER_ID = 1
+    this.http.get<{ 
+      httpStatusCode: number, 
+      responseMessage: string, 
+      users: User 
+      }>(environment.backendUrl+environment.userIdEndpoint+USER_ID)
       .subscribe((data) => {
         console.log(data)
         this.userProfile.next(data.users[0])
@@ -27,8 +32,11 @@ export class UserprofileService {
   }
 
   getRestaurantByUser(userId: number) {
-    const url = 'http://localhost:8080/api/user/restaurant/' + userId;
-    this.http.get<{ httpStatusCode: number, responseMessage: string, restaurants: Restaurant[] }>(url)
+    this.http.get<{ 
+      httpStatusCode: number, 
+      responseMessage: string, 
+      restaurants: Restaurant[] 
+    }>(environment.backendUrl+environment.userRestaurantEndpoint+userId)
       .subscribe(
         (data) => {
           this.restaurantProfile.next(data.restaurants[0])
@@ -37,16 +45,19 @@ export class UserprofileService {
   }
 
   addBench(data: any) {
-    console.log(data)
-    const url = 'http://localhost:8080/api/bench/create'
-    this.http.post(url, data).subscribe((data) => {
+    this.http.post(
+      environment.backendUrl+environment.benchCreateEndpoint, 
+      data).subscribe((data) => {
       console.log(data)
     })
   }
 
   getAllBenches(id: number) {
-    const url = 'http://localhost:8080/api/bench/restaurant/' + id;
-    this.http.get<{ httpStatusCode: number, responseMessage: string, benches: Table[] }>(url).subscribe((data) => {
+    this.http.get<{
+      httpStatusCode: number,
+      responseMessage: string,
+      benches: Table[] 
+    }>(environment.backendUrl+environment.benchAllEndpoint+id).subscribe((data) => {
       this.benchs = data.benches;
       this.benchList.next(this.benchs.slice())
     })
